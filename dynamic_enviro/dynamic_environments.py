fa803b4fefb0eb8eac71ea0dcfcdb2581a3b1a27
vpreston <victorialynnpreston@gmail.com>
2014-10-12 21:58:35 -0400

    Updated docstring

--- dynamic_enviro/dynamic_environments.py
+++ dynamic_enviro/dynamic_environments.py
@@ -14,6 +14,8 @@ October 7, 2014 - All: In class we got everyone's workspace up and running.  The
 October 10, 2014 - All: In class Amanda worked on getting dynamic obstacles develped in Gazebo.  Claire and Victoria worked on adjusting the map display, and identifying the spaces in which Bayesian updates should be made and developed.  In the evening we played with the bayesian equations and attempted to make a quickly updating map.  This was achieved by updating the hits within the particle for loop, and limiting how 'unlikely' something was with being present.  By doing this, you can create the effect of a more dynamic environment, by assuming some arbitrarily small liklihood that SOMETHING will be in a location, no matter how many times you read it as blank.
 
 October 11, 2014 - C+A: Added dynamic obstacles and second map that has only them plotted on it. Plots on orginal map as well. Obstacles dissapear after 15 cycles.
+
+October 12, 2014 - All: We made the thingy work!  Mapping is still rough, can optimize tomorrow.  Adjusted things like top cap of odds, constants, adjusting pathing, etc.
 """
 #interfaces with ROS and Python
 import rospy
@@ -232,7 +234,7 @@ class RunMapping:
 					self.dyn_obs.append((i,j,self.counter))
 
 				#whoa buddy, a thing just appeared
-				if delta > 100.0 and (i,j) not in self.rapid_appear:
+				if delta > 1000.0 and (i,j) not in self.rapid_appear:
 					self.rapid_appear.add((i,j))
 
 				if self.odds_ratios[i,j] < 1/50.0:
