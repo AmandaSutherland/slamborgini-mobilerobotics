d2dfda7be83adcee83ab9ad05b29d561c3338d62
vpreston <victorialynnpreston@gmail.com>
2014-10-14 00:42:13 -0400

    Updated comments for clarity

--- dynamic_enviro/dynamic_environments.py
+++ dynamic_enviro/dynamic_environments.py
@@ -19,13 +19,11 @@ import cv2 #openCV used to make window for publishing data
 import numpy as np
 from numpy.random import random_sample
 from matplotlib.pyplot import imshow
-from sklearn.neighbors import NearestNeighbors
 
 class TransformHelpers:
 	""" Helper functions for making transformations of data from the RunMapping
 	world to the script and back.  Will only be useful for us if we add an autonomy
-	function to the robot """
-
+	function to the robot. Credits to Paul Ruvulo """
 
 	@staticmethod
 	def convert_pose_to_xy_and_theta(pose):
@@ -35,11 +33,8 @@ class TransformHelpers:
 		return (pose.position.x, pose.position.y, angles[2])
 
 class RunMapping:
-	""" Stores an occupancy field for an input map.  An occupancy field returns the distance to the closest
-		obstacle for any coordinate in the map
-		Attributes:
-			map: the map to localize against (nav_msgs/OccupancyGrid)
-			closest_occ: the distance for each entry in the OccupancyGrid to the closest obstacle
+	""" Creates a display for an Occupancy Field map
+		Tracks the robot's position, can be expanded to track cluster positions
 	"""
 
 	def __init__(self):
@@ -63,16 +58,12 @@ class RunMapping:
 		self.p_occ = 0.5*np.ones((self.n, self.n)) #50-50 chance of being occupied
 		self.odds_ratio_hit = 3.0 #this is arbitrary, can re-assign
 		self.odds_ratio_miss = 0.3 #this is arbitrary, can reassign
-		#TODO: Evaluate these - what do we need to change in order to make this more friendly to our version?  Potential changes:
-		#Whenever there is an adjustment to self.odds_ratio_miss, update an odds ratio that implies dynamicness
-
 
 		#calculates odds based upon hit to miss, equal odds to all grid squares
 		self.odds_ratios = (self.p_occ)/(1-self.p_occ)*np.ones((self.n, self.n))
 		#calculate initial past odds_ratio
 		self.past_odds_ratios = (self.p_occ)/(1-self.p_occ)*np.ones((self.n, self.n))
-		#TODO: Make sure that this is still an accurate representation of how probabilities work in our system.  Make appropriate additions/adjustments for the dynamic obstacle piece
-		
+
 		#write laser pubs and subs
 		rospy.Subscriber("scan", LaserScan, self.scan_received, queue_size=1)
 		self.pub = rospy.Publisher("map", OccupancyGrid)
@@ -97,6 +88,7 @@ class RunMapping:
 		# convert the odom pose to the tuple (x,y,theta)
 		self.odom_pose = TransformHelpers.convert_pose_to_xy_and_theta(self.odom_pose.pose)
 
+		#local variable used to take odds samples for future comparisons
 		time_past = 0
 
 		for degree in range(360):
@@ -141,9 +133,7 @@ class RunMapping:
 							self.past_odds_ratios[x_ind, y_ind]=self.odds_ratios[x_ind, y_ind]
 							time_past += 1
 						self.odds_ratios[x_ind, y_ind] *= self.p_occ[x_ind, y_ind] / (1-self.p_occ[x_ind, y_ind]) * self.odds_ratio_miss
-						#self.p_occ[x_ind, y_ind] *= self.p_occ[x_ind, y_ind] * self.odds_ratio_miss/self.odds_ratio_hit
 						marked.add((x_ind, y_ind))
-						#print 'New Point'
 				if not(self.is_in_map(data_x, data_y)) and self.odds_ratios[data_x, datay_pixel] >= 1/60.0:
 					#if it is not in the map, update the odds of hitting it
 					if time_past % 5 == 0:
@@ -169,25 +159,26 @@ class RunMapping:
 				#this is giving us the i,j grid square, occupancy grid
 				for j in range(self.n):
 					idx = i+self.n*j #makes horizontal rows (i is x, j is y)
-					if self.odds_ratios[i,j] < 1/5.0:
+					if self.odds_ratios[i,j] < 1/50.0:
 						map.data[idx] = 0 #makes the gray
-					elif self.odds_ratios[i,j] >= 1/5.0 < 0.5:
+					elif self.odds_ratios[i,j] >= 1/50.0 < 4.0/5.0:
 						map.data[idx] = 25
-					elif self.odds_ratios[i,j] > 0.5:
+					elif self.odds_ratios[i,j] > 50.0:
 						map.data[idx] = 100 #makes the black walls
 					else:
 						map.data[idx] = -1 #makes unknown
 			self.pub.publish(map)
-			#TODO: Change display such that we're not just looking at the ratio, but mapping the dynamic archive and current readings
 
 		image = np.zeros((self.odds_ratios.shape[0], self.odds_ratios.shape[1],3))
 		image2 = np.zeros((self.odds_ratios.shape[0], self.odds_ratios.shape[1],3))
 
 		self.counter+=1
 
+		#get robots position
 		x_odom_index = int((self.odom_pose[0] - self.origin[0])/self.resolution)
 		y_odom_index = int((self.odom_pose[1] - self.origin[1])/self.resolution)
 
+		#remember robots position over time
 		self.pose.append((x_odom_index, y_odom_index))
 
 		#.shape() comes from being related to the np class
@@ -197,29 +188,27 @@ class RunMapping:
 				delta = (self.odds_ratios[i,j]-self.past_odds_ratios[i,j])
 				if (delta < 0.0) and (i,j) in self.rapid_appear:
 					self.dyn_obs.append((i,j,self.counter))
-
-				#whoa buddy, a thing just appeared
+				#whoa buddy, a thing just appeared!
 				if delta > 1000.0 and (i,j) not in self.rapid_appear:
 					self.rapid_appear.add((i,j))
-
+				#Map the world with particular colors
 				if self.odds_ratios[i,j] < 1/50.0:
 					image[i,j,:] = 1.0 #makes open space
 				elif self.odds_ratios[i,j] >= 1/50.0 and self.odds_ratios[i,j] <4/5.0:
-					image[i,j,:] = (0, 255, 0)
+					image[i,j,:] = (0, 255, 0) #uncertainty points
 				elif self.odds_ratios[i,j] > 50.0:
 					image[i,j,:] = (0, 0, 255) #makes walls
 				else:
 					image[i,j,:] = 0.5 #not read
 					
-
+		#if a dynamic obstacle is recorded, let's see it!
 		if len(self.dyn_obs)>0:
 			for point in self.dyn_obs:
 				if (self.counter-point[2])<=10:
 					image2[point[0],point[1]] = (255,0,255) #makes old/dynamic shapes on other map
-
+		#also, we want to map the robot's trail
 		for point in self.pose:
 			image[point[0], point[1]] = (255, 0, 0)
-
 
 		#draw it!
 		cv2.circle(image,(y_odom_index, x_odom_index), 2,(255,0,0))
