8ce78a1e48709bce86e92e433fcebb379aa39b91
vpreston <victorialynnpreston@gmail.com>
2014-10-14 00:49:50 -0400

    Updated comment string in simple cluster code

--- dynamic_enviro/dynam_simple_clusters.py
+++ dynamic_enviro/dynam_simple_clusters.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 
-"""
-October 3, 2014 - Adjustments were made to Paul's particle filter code 
-to gather all the statements which interact with gazebo, rviz, or 
-various rostopics.  The rest is a pseudo code structure.
-
-October 4, 2014 - C+V: Finished formatting base code form Paul's inclass example (with some minor adjustments and renamings, and from pf_code that seemed useful, especially the transforms). Tested code - results was: it worked!
-
-October 6, 2014 - C+A: Reviewed code so far, attempted to implement on computers.  Serious issues with launching it on their computers.
-
-October 7, 2014 - All: In class we got everyone's workspace up and running.  Then we wrote pseudocode to implement at a meeting to be held later today.  At that next meeting we discussed the use of Bayes.  We decided to keep the current bayes set-up, and played with the spectrum.  We have a plan on logging data into a database once it is read, and as it fades the new color of the database log will come through.  After this, we will work on robot localization, by setting up a simple particle filter (maybe) and relate it to our generated map.
-
-October 10, 2014 - All: In class Amanda worked on getting dynamic obstacles develped in Gazebo.  Claire and Victoria worked on adjusting the map display, and identifying the spaces in which Bayesian updates should be made and developed.  In the evening we played with the bayesian equations and attempted to make a quickly updating map.  This was achieved by updating the hits within the particle for loop, and limiting how 'unlikely' something was with being present.  By doing this, you can create the effect of a more dynamic environment, by assuming some arbitrarily small liklihood that SOMETHING will be in a location, no matter how many times you read it as blank.
-
-October 11, 2014 - C+A: Added dynamic obstacles and second map that has only them plotted on it. Plots on orginal map as well. Obstacles dissapear after 15 cycles.
-
-October 12, 2014 - All: We made the thingy work!  Mapping is still rough, can optimize tomorrow.  Adjusted things like top cap of odds, constants, adjusting pathing, etc.
-"""
 #interfaces with ROS and Python
 import rospy
 #Getting topics from ROS
@@ -41,8 +24,7 @@ from sklearn.neighbors import NearestNeighbors
 class TransformHelpers:
 	""" Helper functions for making transformations of data from the RunMapping
 	world to the script and back.  Will only be useful for us if we add an autonomy
-	function to the robot """
-
+	function to the robot. Credits to Paul Ruvulo. """
 
 	@staticmethod
 	def convert_pose_to_xy_and_theta(pose):
@@ -51,14 +33,9 @@ class TransformHelpers:
 		angles = euler_from_quaternion(orientation_tuple)
 		return (pose.position.x, pose.position.y, angles[2])
 
-
-""" Difficulty Level 2 """
 class RunMapping:
-	""" Stores an occupancy field for an input map.  An occupancy field returns the distance to the closest
-		obstacle for any coordinate in the map
-		Attributes:
-			map: the map to localize against (nav_msgs/OccupancyGrid)
-			closest_occ: the distance for each entry in the OccupancyGrid to the closest obstacle
+	""" Creates a display for an Occupancy Field map
+		Tracks the robot's position, can be expanded to track cluster positions
 	"""
 
 	def __init__(self):
@@ -72,7 +49,6 @@ class RunMapping:
 		self.n = 200
 
 		self.pose = []
-
 		self.dyn_obs=[]
 		self.obstacle = []
 		self.rapid_appear = set()
@@ -82,16 +58,12 @@ class RunMapping:
 		self.p_occ = 0.5*np.ones((self.n, self.n)) #50-50 chance of being occupied
 		self.odds_ratio_hit = 3.0 #this is arbitrary, can re-assign
 		self.odds_ratio_miss = 0.3 #this is arbitrary, can reassign
-		#TODO: Evaluate these - what do we need to change in order to make this more friendly to our version?  Potential changes:
-		#Whenever there is an adjustment to self.odds_ratio_miss, update an odds ratio that implies dynamicness
-
 
 		#calculates odds based upon hit to miss, equal odds to all grid squares
 		self.odds_ratios = (self.p_occ)/(1-self.p_occ)*np.ones((self.n, self.n))
 		#calculate initial past odds_ratio
 		self.past_odds_ratios = (self.p_occ)/(1-self.p_occ)*np.ones((self.n, self.n))
-		#TODO: Make sure that this is still an accurate representation of how probabilities work in our system.  Make appropriate additions/adjustments for the dynamic obstacle piece
-		
+
 		#write laser pubs and subs
 		rospy.Subscriber("scan", LaserScan, self.scan_received, queue_size=1)
 		self.pub = rospy.Publisher("map", OccupancyGrid)
@@ -99,21 +71,6 @@ class RunMapping:
 		#note - in case robot autonomy is added back in
 		self.tf_listener = TransformListener()	
 	
-	def get_closest_obstacle_distance(self,x,y): #CHANGE TO get_closest_obstacle_path
-		""" Compute the closest obstacle to the specified (x,y) coordinate in the map.  If the (x,y) coordinate
-			is out of the map boundaries, nan will be returned. """
-		pass
-			# x_coord = int((x - self.map.info.origin.position.x)/self.map.info.resolution)
-			# y_coord = int((y - self.map.info.origin.position.y)/self.map.info.resolution)
-			# # check if we are in bounds
-			# if x_coord > self.map.info.width or x_coord < 0:
-			# 	return float('nan')
-			# if y_coord > self.map.info.height or y_coord < 0:
-			# 	return float('nan')
-			# ind = x_coord + y_coord*self.map.info.width
-			# if ind >= self.map.info.width*self.map.info.height or ind < 0:
-			# 	return float('nan')
-			# return self.closest_occ[ind]
 
 	def is_in_map(self, x, y):
 		"Returns boolean of whether or not a point is within map boundaries"
@@ -131,6 +88,7 @@ class RunMapping:
 		# convert the odom pose to the tuple (x,y,theta)
 		self.odom_pose = TransformHelpers.convert_pose_to_xy_and_theta(self.odom_pose.pose)
 
+		#local variable used to take odds samples for future comparisons
 		time_past = 0
 
 		for degree in range(360):
@@ -175,9 +133,7 @@ class RunMapping:
 							self.past_odds_ratios[x_ind, y_ind]=self.odds_ratios[x_ind, y_ind]
 							time_past += 1
 						self.odds_ratios[x_ind, y_ind] *= self.p_occ[x_ind, y_ind] / (1-self.p_occ[x_ind, y_ind]) * self.odds_ratio_miss
-						#self.p_occ[x_ind, y_ind] *= self.p_occ[x_ind, y_ind] * self.odds_ratio_miss/self.odds_ratio_hit
 						marked.add((x_ind, y_ind))
-						#print 'New Point'
 				if not(self.is_in_map(data_x, data_y)) and self.odds_ratios[data_x, datay_pixel] >= 1/60.0:
 					#if it is not in the map, update the odds of hitting it
 					if time_past % 5 == 0:
@@ -203,16 +159,15 @@ class RunMapping:
 				#this is giving us the i,j grid square, occupancy grid
 				for j in range(self.n):
 					idx = i+self.n*j #makes horizontal rows (i is x, j is y)
-					if self.odds_ratios[i,j] < 1/5.0:
+					if self.odds_ratios[i,j] < 1/50.0:
 						map.data[idx] = 0 #makes the gray
-					elif self.odds_ratios[i,j] >= 1/5.0 < 0.5:
+					elif self.odds_ratios[i,j] >= 1/50.0 < 4.0/5.0:
 						map.data[idx] = 25
-					elif self.odds_ratios[i,j] > 0.5:
+					elif self.odds_ratios[i,j] > 50:
 						map.data[idx] = 100 #makes the black walls
 					else:
 						map.data[idx] = -1 #makes unknown
 			self.pub.publish(map)
-			#TODO: Change display such that we're not just looking at the ratio, but mapping the dynamic archive and current readings
 
 		image = np.zeros((self.odds_ratios.shape[0], self.odds_ratios.shape[1],3))
 		image2 = np.zeros((self.odds_ratios.shape[0], self.odds_ratios.shape[1],3))
@@ -227,26 +182,24 @@ class RunMapping:
 		#.shape() comes from being related to the np class
 		for i in range(image.shape[0]):
 			for j in range(image.shape[1]):
-				#print self.past_odds_ratios[i,j]
-				#print self.odds_ratios[i,j]
 				#the thing that just rapidly appeared, disappeared!
 				delta = (self.odds_ratios[i,j]-self.past_odds_ratios[i,j])
 				if (delta < 0.0) and (i,j) in self.rapid_appear:
 					self.dyn_obs.append((i,j,self.counter))
-
-				#whoa buddy, a thing just appeared
+				#whoa buddy, a thing just appeared!
 				if delta > 1000.0 and (i,j) not in self.rapid_appear:
 					self.rapid_appear.add((i,j))
-
+				#reference the odds for coloration
 				if self.odds_ratios[i,j] < 1/50.0:
 					image[i,j,:] = 1.0 #makes open space
 				elif self.odds_ratios[i,j] >= 1/50.0 and self.odds_ratios[i,j] <4/5.0:
-					image[i,j,:] = (0, 255, 0)
+					image[i,j,:] = (0, 255, 0) #uncertainty points
 				elif self.odds_ratios[i,j] > 50.0:
 					image[i,j,:] = (0, 0, 255) #makes walls
 				else:
 					image[i,j,:] = 0.5 #not read
-					
+		
+		#if dynamic obstacles have been recorded, lets find the center of them, and track a path 		
 		sumx = 0
 		sumy = 0
 		if len(self.dyn_obs)>0:
@@ -261,6 +214,7 @@ class RunMapping:
 			self.obstacle.append((avgx, avgy))
 			del self.dyn_obs[:]
 
+		#plot robot and cluster paths over time
 		for point in self.pose:
 			image[point[0], point[1]] = (255, 0, 0)
 		for point in self.obstacle:
